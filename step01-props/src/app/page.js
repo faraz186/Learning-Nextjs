b0948fb3538cdcae30ad4485dbd36f745c4d1c10
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-09-25 00:19:43 +0500

    props from C2C (Component to Component)

---
+++ step01-props/src/app/page.js
@@ -0,0 +1,32 @@
+export default function Home() {
+  return (
+
+    <div style={{textAlign:"center"}}>
+      
+      <h1 style={{textDecoration:"underline"}}>Props</h1>
+
+      <br />
+      <User name="Muhammad Faraz"/>
+      <br />
+
+      <User name="Ali Ahmed"/>
+      <br />
+
+      <User name="Kashif Abbas"/>
+      <br />
+
+      <User name="Muhammad Shabbir "/>
+      <br />
+
+    </div>
+  )
+}
+
+const User = (props) =>{
+  return(
+    <div>
+        <h1>Hello I am <span style={{color:"blue"}}>{props.name}</span></h1>
+    </div>
+  )
+}
+
