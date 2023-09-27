23a9a9056eadcd5ad3877e14890f8860ce8c6fe1
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-09-28 00:28:15 +0500

    routes handler..

---
+++ step08-middleware/src/app/studentlist/[student]/page.js
@@ -0,0 +1,24 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function StudentDetails({ params }) {
+  const router = useRouter();
+  console.log(params);
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Student Details</h1>
+
+      <h3 style={{ textAlign: "center", marginTop: 50 }}>
+        <i>Hello Mr. {params.student}</i>
+      </h3>
+
+      <button
+        onClick={() => router.push("/studentlist")}
+        style={{ position: "absolute", left: 600, top: 180 }}
+      >
+        Back to Student List Page
+      </button>
+    </div>
+  );
+}
