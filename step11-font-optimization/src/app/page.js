7183ff44ce28ee005162a08f8384b557e127e39f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-09-29 00:14:04 +0500

    font optimization using google font..

---
+++ step11-font-optimization/src/app/page.js
@@ -0,0 +1,16 @@
+import { Roboto } from "next/font/google"
+
+const roboto = Roboto({
+  weight:"300",
+  subsets:["latin"],
+  display:"swap"
+})
+
+export default function Home() 
+{
+  return (
+   <>
+    <h1 className={roboto.className}>Font Optimization in Nextjs</h1>
+   </>
+  )
+}
