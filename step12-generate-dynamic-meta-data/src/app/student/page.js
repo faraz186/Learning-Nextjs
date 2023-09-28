d8dae8408c580ef6b6bc473f7bbb87e01337236c
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-09-29 00:17:38 +0500

    dynamic meta data

---
+++ step12-generate-dynamic-meta-data/src/app/student/page.js
@@ -0,0 +1,17 @@
+export default function Student()
+{
+    return(
+        <div>
+            <h1 style={{textAlign:"center"}}>Student Page</h1>
+        </div>
+    )
+}
+
+export function generateMetadata(){
+    return{
+        title:"Student page title",
+        description:"Student Page description"
+
+    }
+    
+}
---
+++ step12-generate-dynamic-meta-data/src/app/user/page.js
@@ -0,0 +1,15 @@
+export default function User()
+{
+    return(
+        <div>
+            <h1 style={{textAlign:"center"}}>User Page</h1>
+        </div>
+    )
+}
+
+export function generateMetadata(){
+    return{
+        title:"User page title",
+        description:"User Page description"
+    }   
+}
