cf880a821a603e99bcaa043cd0f4ec471e6eaec6
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-09-30 14:19:21 +0500

    api call through CP..

---
+++ step13-api_call-clientcomponent/src/app/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <h1>API Calling with Client Component</h1>
+
+      <Link href="/productlist">Go to Product page</Link>
+    </>
+  )
+}
---
+++ step13-api_call-clientcomponent/src/app/productlist/page.js
@@ -0,0 +1,29 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export default function Product()
+{
+    const [product,setProduct] = useState([])
+
+    useEffect(async ()=>{
+        let data = await fetch("https://dummyjson.com/products");
+        data = await data.json();
+        console.log(data.products);
+        setProduct(data.products);
+    })
+    return(
+        <>
+            <h1>Product page</h1>
+            {
+                product.map((e,i)=>{
+                    return(
+                        <div key={i}>
+                            <h3>{e.title}</h3>
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
