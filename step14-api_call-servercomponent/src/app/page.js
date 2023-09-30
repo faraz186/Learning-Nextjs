c459bd16cf7bed7b4d9325c96186f84b41cc31dd
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-09-30 14:20:35 +0500

    api call through SP..

---
+++ step14-api_call-servercomponent/src/app/page.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Home() 
+{
+  return (
+
+    <div style={{marginLeft:"32%"}}>
+
+      <h1>API Calling with Server Component</h1>
+
+      <Link style={{marginLeft:"15%"}} href="/productlist">Go to Product List</Link>
+
+    </div>
+  )
+}
