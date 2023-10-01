e51adb89bab9cffae2a9b5bb585bea620cae313c
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-02 00:26:40 +0500

    api routes..

---
+++ step16b-api-with-routes/src/app/api/user/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export function GET(request) {
+  return NextResponse.json(
+    {
+      name: "Faraz",
+      age: 22,
+      email: "faraz@gmail.com",
+    },
+    { status: 201 }
+  );
+}
