996c5afc86c17373a0b6643ffb222dcc3762c4d8
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-04 09:43:23 +0500

    Delete step-16c-get-api_with-static-data directory

--- step-16c-get-api_with-static-data/src/app/api/users/route.js
+++
@@ -1,8 +0,0 @@
-import { user } from "@/utils/db";
-import { NextResponse } from "next/server";
-
-export function GET()
-{
-    const data = user;
-    return NextResponse.json(data,{status:201})
-}
--- step-16c-get-api_with-static-data/src/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 style={{textAlign:"center"}}>Get Api with Static Data..</h1>
-    </>
-    )
-}
