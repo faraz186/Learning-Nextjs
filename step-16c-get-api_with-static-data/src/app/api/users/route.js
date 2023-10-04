f7ad348381b19496d84435266a9c562361972e82
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-04 09:42:59 +0500

    filter api data..

---
+++ step-16c-get-api_with-static-data/src/app/api/users/route.js
@@ -0,0 +1,8 @@
+import { user } from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export function GET()
+{
+    const data = user;
+    return NextResponse.json(data,{status:201})
+}
