7705b23956646eeb75942a9f655ee9c5da32c43b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-06 00:19:56 +0500

    integrate with CSR..

---
+++ step16f-integrate-post-api/src/app/api/users/[id]/route.js
@@ -0,0 +1,13 @@
+import { user } from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export function GET(request,content)
+{
+    // console.log(content.params.id);
+
+    let userdata = user.filter((item)=>item.id == content.params.id)
+    return NextResponse.json(userdata.length==0?{result:"No data found",success:false}:{result:userdata[0],success:true},{status:200})
+
+    // return NextResponse.json(
+    //     user,{status:200});
+}
---
+++ step16f-integrate-post-api/src/app/users/[userid]/page.js
@@ -0,0 +1,22 @@
+async function getUsers(id)
+{
+    let data = await fetch(`http://localhost:3000/api/users/${id}`);
+    data = await data.json();
+    return data.result;
+}
+
+export default async function Page({params})
+{
+
+    const users = await getUsers(params.userid);
+
+    return(
+        <div>
+            <h1>User Details..</h1>
+            <h4>Name: {users.name}</h4>
+            <h4>Age: {users.age}</h4>
+            <h4>Email: {users.email}</h4>
+
+        </div>
+    )
+}
