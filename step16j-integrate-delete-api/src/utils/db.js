cbb717af435c2746c8a6eae21670e0062b3bdfab
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-08 01:57:47 +0500

    integrate with api with delete

---
+++ step16j-integrate-delete-api/src/utils/db.js
@@ -0,0 +1,31 @@
+export const user = [
+    {
+        id:10,
+        name:"faraz",
+        age:22,
+        email:"faraz@gmail.com"
+    },
+    {
+        id:20,
+        name:"Ashar",
+        age:25,
+        email:"ashar@gmail.com"
+    },
+    {
+        id:30,
+        name:"Ali",
+        age:31,
+        email:"ali@gmail.com"
+    },{
+        id:40,
+        name:"Saqib",
+        age:26,
+        email:"saqib@gmail.com"
+    }
+    ,{
+        id:50,
+        name:"Affan",
+        age:35,
+        email:"affan@gmail.com"
+    }
+]
---
+++ step16j-integrate-delete-api/src/utils/deleteUser.js
@@ -0,0 +1,22 @@
+"use client"
+
+export default function DeleteUser(props)
+{
+    const userId = props.id;
+
+    console.log(userId);
+
+    const deleteUser = async ()=>{
+        let result = await fetch("http://localhost:3000/api/users"+userId,{
+            method:"delete",
+            
+        });
+        result = await result.json();
+
+        if(result.success)
+        {
+            alert("User Deleted...")
+        }
+    }
+    return <button onClick={deleteUser}>Delete</button>
+}
