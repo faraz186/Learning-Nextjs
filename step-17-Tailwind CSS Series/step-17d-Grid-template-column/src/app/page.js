741c1cb564ccfa1539996acbacaf756ab0fe677f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-09 00:35:19 +0500

    Add files via upload

---
+++ step-17-Tailwind CSS Series/step-17d-Grid-template-column/src/app/page.js
@@ -0,0 +1,29 @@
+export default function Home() 
+{
+  return (
+
+    <>
+      <div className="max-w-[1200px] py-5 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 gap-5 text-white mx-auto">
+
+        <div className="bg-green-500 py-5 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 py-5 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 py-5 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 py-5 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 py-5 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 py-5 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 py-5 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+        
+        <div className="bg-green-500 py-5 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+      </div>
+    </>
+
+
+  )
+}
