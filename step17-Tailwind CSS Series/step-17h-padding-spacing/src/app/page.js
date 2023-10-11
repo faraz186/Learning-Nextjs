e78fc63922a425ce0fb0548ded04ef0b0975e788
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-11 23:13:34 +0500

    padding styling..

---
+++ step17-Tailwind CSS Series/step-17h-padding-spacing/src/app/page.js
@@ -0,0 +1,31 @@
+export default function Home() 
+{
+  return (
+      <>
+      <div className="max-w-[1320px] bg-metal-500 text-center p-5 py-10 pb-10 mx-auto">
+        <h1>Welcome to Tailwind CSS</h1>
+      </div>
+      <div className="max-w-[1320px] grid grid-rows-3 gap-5 grid-flow-col mx-auto">
+
+        <div className="bg-green-500  py-5 text-center">1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 row-span-2 py-5 text-center">2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 py-5 text-center">3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 py-5 text-center">4. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 py-5 text-center">5. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 py-5 text-center">6. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+        <div className="bg-green-500 py-5 text-center">7. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+        
+        <div className="bg-green-500 py-5 text-center row-span-4">8. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Eu consequat ac felis donec. Est lorem ipsum dolor sit amet consectetur. Faucibus in ornare quam viverra orci sagittis eu.</div>
+
+      </div>
+    </>
+
+
+  )
+}
