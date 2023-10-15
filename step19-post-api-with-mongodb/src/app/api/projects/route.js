9cc51d935a31a80978bf519df6c3848db2329297
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-15 19:44:39 +0500

    post data from database..

---
+++ step19-post-api-with-mongodb/src/app/api/projects/route.js
@@ -0,0 +1,33 @@
+import {NextResponse} from 'next/server';
+import mongoose from "mongoose";
+import {connectionSrt} from "@/lib/db"
+import { Product } from '@/lib/model/products';
+
+export async function GET()
+{
+    let data = [];
+    try{
+        await mongoose.connect(connectionSrt);
+        data = await Product.find();
+    }
+    catch(err){
+        data={success:false}
+    }
+
+    return NextResponse.json({result:data,success:true})
+}
+
+export async function POST(request)
+{
+    const payload = await request.json();
+
+    await mongoose.connect(connectionSrt);
+
+    let product = new Product(payload);
+
+    const result = await product.save();
+
+    return NextResponse.json({result,success:true})
+
+
+}
