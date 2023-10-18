466e69c6eb39da7740dc0dd15021baa9863d6c4b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-19 00:11:33 +0500

    Add files via upload

---
+++ step22-put-api-with-mongodb/src/app/page.js
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+import Link from 'next/link'
+export default function Home()
+{
+    return(
+        <main className={styles.main}>
+            <Link href="/products">Products</Link>
+            <Link href="/addproduct">Add Products</Link>
+        </main>
+    )
+}
