a9a34db1cd75b81558a11d1f6f998c317de9c74b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-12-08 01:36:04 +0500

    Update page.js

--- step03b-nested-routing/src/app/page.js
+++ step03b-nested-routing/src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Nested Routing | Home Page </h1>
+      <h1 style={{ textAlign: "center" }}> Nested Routing | Home Page </h1>
     </div>
   );
 }
