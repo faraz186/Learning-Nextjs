819e50a99a216bba8c15560e7a542e76c84218cd
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-12-08 01:37:28 +0500

    Update page.js

--- step03b-nested-routing/src/app/login/loginteacher/page.js
+++ step03b-nested-routing/src/app/login/loginteacher/page.js
@@ -1,7 +1,7 @@
 export default function LoginTeacher() {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Login Teacher Page</h1>
+      <h1 style={{ textAlign: "center" }}>Login Teacher Page </h1>
     </div>
   );
 }
