0ec7a46b80349e71c2429836d667e91e7a1835d6
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-14 10:02:38 +0500

    Update page.js

--- step25b-createStore &  Provider/src/app/page.js
+++ step25b-createStore &  Provider/src/app/page.js
@@ -95,7 +95,7 @@ export default function Home() {
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener noreferrer"    
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Deploy{' '}
