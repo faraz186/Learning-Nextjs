5fd88ddd6d8fccdf59ecbcc80967baf262322301
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-14 10:01:40 +0500

    Update page.js

--- step25b-createStore &  Provider/src/app/page.js
+++ step25b-createStore &  Provider/src/app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
           width={180}
           height={37}
           priority
-        />
+        />   
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
