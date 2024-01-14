a34523b3f6beeef7965a78bafb31c77e58cbd63f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-14 10:03:02 +0500

    Update layout.js

--- step25b-createStore &  Provider/src/app/layout.js
+++ step25b-createStore &  Provider/src/app/layout.js
@@ -15,6 +15,6 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Provider>{children}</Provider>
       </body>
-    </html>
+    </html>        
   );
 }
