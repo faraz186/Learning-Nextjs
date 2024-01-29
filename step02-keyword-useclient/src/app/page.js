c8058963dbf3696654197f06ab16fd7f7223f10c
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-30 00:59:39 +0500

    Update page.js

--- step02-keyword-useclient/src/app/page.js
+++ step02-keyword-useclient/src/app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
   return (
     <div>
       <h1>Hello, {name}</h1>
-
+  
       <button style={{ marginRight: 20 }} onClick={popUp}>
         Change my name
       </button>
