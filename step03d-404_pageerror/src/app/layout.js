8ccc021f87e36303b543b9ff54d9d74bb35db9e3
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-02-01 00:36:36 +0500

    Update layout.js

--- step03d-404_pageerror/src/app/layout.js
+++ step03d-404_pageerror/src/app/layout.js
@@ -11,6 +11,6 @@ export default function RootLayout({ children }) {
   return (  
     <html lang="en">
       <body className={inter.className}>{children}</body>   
-    </html>
+    </html>  
   )
 }
