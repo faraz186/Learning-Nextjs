e8330696f07d7927f42cb6709df5b64d31454a83
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-03-03 13:37:44 +0500

    Update page.js

--- step05b-conditional-layout/src/app/about/page.js
+++ step05b-conditional-layout/src/app/about/page.js
@@ -8,7 +8,7 @@ const About=()=>{
         <div>
             <h1>About Page</h1>
             <button onClick={()=>router.push("/")} >Go to Home Page</button>
-
+   
         <br />
         <Link href="/about/aboutcollege" >Go to About College page</Link>
         <br /> <br />
