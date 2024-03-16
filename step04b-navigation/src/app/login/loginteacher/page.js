d6165fd5aad0914b05c7199f5e54dac5036611c2
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-03-16 19:12:43 +0500

    Update page.js

--- step04b-navigation/src/app/login/loginteacher/page.js
+++ step04b-navigation/src/app/login/loginteacher/page.js
@@ -21,9 +21,10 @@ export default function LoginTeacher() {
       <button onClick={() => router.push("/login/loginteacher")}>
         Go to Login Teacher Page
       </button>
-
+    
       <br />
       <br />
+  
       <button onClick={() => router.push("/about/aboutcollege")}>
         Go to About College Page
       </button>
