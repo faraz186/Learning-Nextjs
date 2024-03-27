bc09949db8ef356b17144845a4bef182b7b75239
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-03-27 17:04:28 +0500

    Update page.js

--- step10-image-optimization/src/app/page.js
+++ step10-image-optimization/src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
 
     <h2 style={{textAlign:"center",textDecoration:"underline"}}>Offline Image</h2>
     
-    <Image style={{marginLeft:"40%"}} src={img} 
+    <Image style={{marginLeft:"40%"}} src={img}    
     width={400} height={250} />
     </>
   )
