811b1079c7f242de1af0ae22d25eeb844114fe10
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-03-27 17:01:33 +0500

    Update page.js

--- step03-basic-routing/src/app/login/page.js
+++ step03-basic-routing/src/app/login/page.js
@@ -1,7 +1,7 @@
 const Login = () => {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Login Page</h1>
+      <h1 style={{ textAlign: "center" }}>Login Page</h1>  
     </div>   
   );
 };
