a6c4a8d686f2417b5d9897b77c71cd9a1e48c714
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-04-04 00:31:43 +0500

    Update middleware.js

--- step08-middleware/src/middleware.js
+++ step08-middleware/src/middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 
 export default function middleware(request){
-    return NextResponse.redirect(new URL("/login",request.url));
+    return NextResponse.redirect(new URL("/login",request.url));  
 }
         
 export const config={
