3a163446bf31c6cfed52ff44f6c0909c166652d3
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-04-04 00:31:59 +0500

    Update middleware.js

--- step08-middleware/src/middleware.js
+++ step08-middleware/src/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 
 export default function middleware(request){
     return NextResponse.redirect(new URL("/login",request.url));  
-}
+}   
         
 export const config={
     matcher:"/about/:path*"
