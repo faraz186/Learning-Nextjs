5850c91d8eff7f5903c875b03d7d73407b46431b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-04-17 18:40:27 +0500

    Update page.js

--- step13-api_call-clientcomponent/src/app/productlist/page.js
+++ step13-api_call-clientcomponent/src/app/productlist/page.js
@@ -12,7 +12,7 @@ export default function Product()
         console.log(data.products);   
         setProduct(data.products);   
     })
-    return(  
+    return(    
         <>
             <h1>Product page</h1>   
             {   
