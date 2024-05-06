c3c4bfeea23ec874b5c6537aed0d9e63c05290c6
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-05-07 00:03:55 +0500

    Update page.js

--- step04a-linking/src/app/about/aboutstudent/page.js
+++ step04a-linking/src/app/about/aboutstudent/page.js
@@ -12,7 +12,7 @@ export default function AboutStudent() {
       <Link href="/about">Go to About Page</Link>
       <br />    
       <br />
-
+   
       <Link href="/login/loginstudent">Go to Login Student Page</Link>
       <br />
       <br />
