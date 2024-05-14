3b43bf78a444c31e28e8c01524bba0a8051222e3
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-05-15 00:34:54 +0500

    Update page.js

--- step03b-nested-routing/src/app/about/aboutstudent/page.js
+++ step03b-nested-routing/src/app/about/aboutstudent/page.js
@@ -1,7 +1,7 @@
 export default function AboutStudent() {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>About Student Page</h1>
+      <h1 style={{ textAlign: "center" }}>About Student Page</h1>  
     </div>
   );
 }
