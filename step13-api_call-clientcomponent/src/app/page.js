52cd76b3df15f597234fdff529c1dbfbcf9221a6
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-05-25 22:18:57 +0500

    Update page.js

--- step13-api_call-clientcomponent/src/app/page.js
+++ step13-api_call-clientcomponent/src/app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {    
   return (
     <>
-      <h1>API Calling with Client Component</h1>
+      <h1>API Calling with Client Component</h1>  
   
       <Link href="/productlist">Go to Product page</Link>
     </>   
