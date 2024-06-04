efa3e6348bf8b86ec135a4ed0dcaa992b7ebf130
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-06-04 13:33:59 +0500

    Update page.js

--- step13-api_call-clientcomponent/src/app/productlist/page.js
+++ step13-api_call-clientcomponent/src/app/productlist/page.js
@@ -1,7 +1,7 @@
 "use client"                    
                
 import { useEffect, useState } from "react"    
-  
+    
 export default function Product()      
 {    
     const [product,setProduct] = useState([])   
