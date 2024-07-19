f5caeb1c75be4735c5a69cd77d04b28b4f237b18
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-07-20 00:48:12 +0500

    Update page.js

--- step25c-Component with Redux/src/app/page.js
+++ step25c-Component with Redux/src/app/page.js
@@ -2,7 +2,7 @@ import AddUsers from "@/components/AddUsers";
 import DisplayUsers from "@/components/DisplayUsers";  
 
 export default function Home() {   
-  return (
+  return (   
     <>
       <AddUsers />   
    
