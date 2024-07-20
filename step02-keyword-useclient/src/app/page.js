c0c593b32662ec7a492cece3c98bfcf942e39635
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-07-21 00:57:01 +0500

    Update page.js

--- step02-keyword-useclient/src/app/page.js
+++ step02-keyword-useclient/src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   const popUp = () => {  
     setName("Muhammad");  
   };           
-   
+     
   const PreviousPopUp = () => {
     setName("Faraz");   
   };     
