4c5df037e0304dd42db532f7f15c7a532dbd9a4e
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-07-30 09:14:10 +0500

    Update page.js

--- step00-helloworld/src/app/page.js
+++ step00-helloworld/src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {        
   return (         
-    <div>               
+    <div>                  
         <h1>Hello User, I am Muhammad Faraz...</h1>         
     </div>       
   )     
