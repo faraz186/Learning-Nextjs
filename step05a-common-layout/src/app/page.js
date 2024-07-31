12270e721e1f56057d31674a7ed836ff7136960f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-01 00:06:52 +0500

    Update page.js

--- step05a-common-layout/src/app/page.js
+++ step05a-common-layout/src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
       <Link href="/about">Go to About Page</Link>
       <br />   
       <br />
-      <button onClick={() => navigate("/login")}>Go to Login Page</button>
+      <button onClick={() => navigate("/login")}>Go to Login Page</button>   
       <button onClick={() => navigate("/about")}>Go to About Page</button>
     </main>
   );
