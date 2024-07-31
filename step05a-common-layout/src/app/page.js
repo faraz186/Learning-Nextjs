cafd712b0d4daad31e180d51b5cd027f45e36c89
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-01 00:06:45 +0500

    Update page.js

--- step05a-common-layout/src/app/page.js
+++ step05a-common-layout/src/app/page.js
@@ -2,7 +2,7 @@
     
 import Link from "next/link";
 import styles from "./page.module.css";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";   
 
 export default function Home() {   
   const router = useRouter();
