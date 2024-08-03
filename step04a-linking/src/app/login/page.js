d5159e457e72f410a2f4a686c3f1c7f38e8fdd95
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-04 03:27:47 +0500

    Update page.js

--- step04a-linking/src/app/login/page.js
+++ step04a-linking/src/app/login/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Login() {   
   return (
     <div style={{ textAlign: "center" }}>
-      <h1 style={{ textAlign: "center" }}>Login Page</h1>
+      <h1 style={{ textAlign: "center" }}>Login Page</h1>   
    
       <Link href="/login">Go to Login Page</Link>
 
