db30df28359f036a4a80007243a1c6b39140e126
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-11 01:59:12 +0500

    Update page.js

--- step05a-common-layout/src/app/about/page.js
+++ step05a-common-layout/src/app/about/page.js
@@ -7,7 +7,7 @@ const About=()=>{
     const router = useRouter();
     return(
         <div>
-            <h1>About Page</h1>
+            <h1>About Page</h1>   
             <button onClick={()=>router.push("/")} >Go to Home Page</button>
 
         <br />  
