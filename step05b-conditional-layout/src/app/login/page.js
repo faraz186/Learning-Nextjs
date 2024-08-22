7569e70115fd1ce07760bb83dbb3e3280f5f8b4c
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-22 09:46:42 +0500

    Update page.js

--- step05b-conditional-layout/src/app/login/page.js
+++ step05b-conditional-layout/src/app/login/page.js
@@ -10,10 +10,15 @@ const Login = () => {
     return (
         <div>
             <h1 className="heading">Login Page</h1>
+        
             <Link href="/" >Go to Home Page</Link>
+        
             <br /> <br />
+        
             <button onClick={() => navigate("loginstudent")} >Go to Student Login Page</button>
+        
             <br /> <br />  
+        
             <button onClick={() => navigate("loginteacher")} >Go to Teacher Login Page</button>
    
         </div>
