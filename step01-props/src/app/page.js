3362ee9d3b044340a67ced800f5bc79022e5e4d2
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-28 13:26:23 +0500

    Update page.js

--- step01-props/src/app/page.js
+++ step01-props/src/app/page.js
@@ -1,5 +1,5 @@
 export default function Home() {      
-  return (                       
+  return (                         
               
     <div style={{textAlign:"center"}}>      
               
