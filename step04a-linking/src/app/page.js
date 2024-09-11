bd96bab24611be3385c35ae7f5a3b552bbd189a3
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-11 12:50:09 +0500

    Update page.js

--- step04a-linking/src/app/page.js
+++ step04a-linking/src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Link href="/login">Go to Login Page</Link>   
 
       <br />
-      <br />  
+      <br />     
    
       <Link href="/about">Go to About Page</Link>     
       
