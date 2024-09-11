aef19a63193e8d38f0b620b44a6b51b479cdc95d
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-11 12:50:49 +0500

    Update page.js

--- step04a-linking/src/app/page.js
+++ step04a-linking/src/app/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";          
                  
-export default function Home() {     
+export default function Home() {        
   return (       
     <div style={{ textAlign: "center" }}>   
   
