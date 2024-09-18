fb3c665d7214bb41ae3d888a32fb4c1253953ebc
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-19 01:03:15 +0500

    Update page.js

--- step03b-nested-routing/src/app/login/loginstudent/page.js
+++ step03b-nested-routing/src/app/login/loginstudent/page.js
@@ -1,4 +1,4 @@
-export default function LoginStudent() {
+export default function LoginStudent() {   
   return (     
     <div>
       <h1 style={{ textAlign: "center" }}>Login Student Page</h1>   
