afc3c0e017a5068bf6c657070a63ef224ce06d38
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-23 01:05:23 +0500

    Update page.js

--- step04b-navigation/src/app/page.js
+++ step04b-navigation/src/app/page.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/navigation";
 
 
 export default function Home() {  
-  const router = useRouter();   
+  const router = useRouter();     
      
   return (  
     <div style={{ textAlign: "center" }}>  
