28bd36c38612a7f6006d106e8ebb0d44859c2395
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-23 01:06:11 +0500

    Update page.js

--- step04b-navigation/src/app/page.js
+++ step04b-navigation/src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
       <h1 style={{ textAlign: "center" }}>Navigation | Home Page</h1>     
       <br />
 
-      <button onClick={() => router.push("/login")}>Go to Login Page</button>  
+      <button onClick={() => router.push("/login")}>Go to Login Page</button>    
   
       <br />
       <br />
