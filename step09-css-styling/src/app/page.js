99554e7bb08cb1bea4db84c4c18b2eaa3bed51ab
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-04 00:36:29 +0500

    Update page.js

--- step09-css-styling/src/app/page.js
+++ step09-css-styling/src/app/page.js
@@ -15,7 +15,7 @@ export default function Home()
       <h3 id={style.orange} >Heading 3</h3>
 
       <h4  className={red}>Dummy text 1</h4>
-      <h4 className={red}>Dummy text</h4>  
+      <h4 className={red}>Dummy text 2</h4>  
       <h4 className={red}>Dummy text</h4>
       <h4 className={red}>Dummy text</h4>    
 
