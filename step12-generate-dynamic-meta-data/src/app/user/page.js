53fccf51d74cc19df96b43faaab9d331c2ef3738
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-07 12:17:10 +0500

    Update page.js

--- step12-generate-dynamic-meta-data/src/app/user/page.js
+++ step12-generate-dynamic-meta-data/src/app/user/page.js
@@ -2,7 +2,7 @@ export default function User()
 {  
     return(  
         <div>
-            <h1 style={{textAlign:"center"}}>User Page</h1>
+            <h1 style={{textAlign:"center"}}>User Page</h1>  
         </div>        
     )
 }  
