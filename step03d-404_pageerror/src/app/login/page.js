a4784cfacb7e8672bd68a7ebc1823b7fe91d88b0
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-09 00:11:10 +0500

    Update page.js

--- step03d-404_pageerror/src/app/login/page.js
+++ step03d-404_pageerror/src/app/login/page.js
@@ -1,5 +1,5 @@
 export default function Login()     
-{                    s        
+{                    
   return(                         
         <>                    
             <h1>Login Page </h1>                     
