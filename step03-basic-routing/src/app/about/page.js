e0abde3e98b2bf7356cae056389a4c8f2741bf6a
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-14 00:15:03 +0500

    Update page.js

--- step03-basic-routing/src/app/about/page.js
+++ step03-basic-routing/src/app/about/page.js
@@ -1,7 +1,7 @@
 const About = () => {        
   return (                 
     <div>           
-      <h1 style={{ textAlign: "center" }}>About Page</h1>         
+      <h1 style={{ textAlign: "center" }}>About Page</h1>           
     </div>         
   );                  
 };        
