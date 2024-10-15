358e5dc97d8866519cf849c553ad667d498149cb
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-15 09:15:37 +0500

    Update page.js

--- step16a-loader_with-api-data/src/app/page.js
+++ step16a-loader_with-api-data/src/app/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";      
 
 export default function Home() {    
-  return (  
+  return (    
     <main>     
       <h1>Loader with API in Next.JS</h1>  
     </main>        
