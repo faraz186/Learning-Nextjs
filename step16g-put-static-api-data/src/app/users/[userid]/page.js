7db38d35b26bcc5f84da7bdfc048e43f5f7e1d5a
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-19 14:13:51 +0500

    Update page.js

--- step16g-put-static-api-data/src/app/users/[userid]/page.js
+++ step16g-put-static-api-data/src/app/users/[userid]/page.js
@@ -2,7 +2,7 @@ async function getUsers(id)
 {
     let data = await fetch(`http://localhost:3000/api/users/${id}`);
     data = await data.json();
-    return data.result;
+    return data.result;   
 }
 
 export default async function Page({params})
