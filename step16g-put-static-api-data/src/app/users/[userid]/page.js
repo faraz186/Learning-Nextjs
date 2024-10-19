0df8807ced7de00a830a96ddee2b36f2b650b55e
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-19 14:13:59 +0500

    Update page.js

--- step16g-put-static-api-data/src/app/users/[userid]/page.js
+++ step16g-put-static-api-data/src/app/users/[userid]/page.js
@@ -11,7 +11,7 @@ export default async function Page({params})
     const users = await getUsers(params.userid);
 
     return(
-        <div>
+        <div>  
             <h1>User Details..</h1>
             <h4>Name: {users.name}</h4>   
             <h4>Age: {users.age}</h4>
