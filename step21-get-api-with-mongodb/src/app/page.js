a3f227fd4016fc17cf513044fc807bbd16116b17
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-20 01:36:23 +0500

    Update page.js

--- step21-get-api-with-mongodb/src/app/page.js
+++ step21-get-api-with-mongodb/src/app/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home()
 {
     return(
-        <main className={styles.main}>
+        <main className={styles.main}>  
             <Link href="/products">Products</Link>  
             <Link href="/addproduct">Add Products</Link>
         </main>
