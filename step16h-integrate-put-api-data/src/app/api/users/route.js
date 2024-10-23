d5ba77bec617a4bbf6a8b5cd94e14ccdf1b4c27c
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-23 14:24:17 +0500

    Update route.js

--- step16h-integrate-put-api-data/src/app/api/users/route.js
+++ step16h-integrate-put-api-data/src/app/api/users/route.js
@@ -2,7 +2,7 @@ import { user } from "@/utils/db";
 import { NextResponse } from "next/server";
 
 export function GET()
-{
+{  
     const data = user;
     return NextResponse.json(data,{status:201})
 }
