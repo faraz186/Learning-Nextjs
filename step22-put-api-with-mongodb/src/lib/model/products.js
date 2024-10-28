e83a37eec633c91cdc366be5740d5feab8684526
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-29 00:18:46 +0500

    Update products.js

--- step22-put-api-with-mongodb/src/lib/model/products.js
+++ step22-put-api-with-mongodb/src/lib/model/products.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";      
 
 const productModel = new mongoose.Schema({  
-    name:String,    
+    name:String,      
     price:String,
     company:String,
     color:String,    
