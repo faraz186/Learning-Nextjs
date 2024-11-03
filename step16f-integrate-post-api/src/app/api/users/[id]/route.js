7ab1c21096bd309094239f3ad45e165664865560
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-11-03 17:39:41 +0500

    Update route.js

--- step16f-integrate-post-api/src/app/api/users/[id]/route.js
+++ step16f-integrate-post-api/src/app/api/users/[id]/route.js
@@ -8,6 +8,6 @@ export function GET(request,content)
     let userdata = user.filter((item)=>item.id == content.params.id)
     return NextResponse.json(userdata.length==0?{result:"No data found",success:false}:{result:userdata[0],success:true},{status:200})
 
-    return NextResponse.json(
+    return NextResponse.json(  
         user,{status:200});  
 }      
