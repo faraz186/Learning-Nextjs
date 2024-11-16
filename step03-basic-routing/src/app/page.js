276359b04d063099a96882f451cf354bcb258eaf
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-11-17 02:08:53 +0500

    Update page.js

--- step03-basic-routing/src/app/page.js
+++ step03-basic-routing/src/app/page.js
@@ -2,6 +2,6 @@ export default function Home() {
   return (  
     <div>  
       <h1 style={{ textAlign: "center" }}>Basic Routing | Main Home Page</h1>    
-    </div>  
+    </div>    
   );       
 }    
