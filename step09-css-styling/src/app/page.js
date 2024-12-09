73b5936d9de26d590481f47053cd515933a87cc3
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-12-10 00:31:59 +0500

    css custom styling..

--- step09-css-styling/src/app/page.js
+++ step09-css-styling/src/app/page.js
@@ -10,7 +10,7 @@ export default function Home()
   return (    
     <main>      
       <h1 className={color=='red'?style.red:style.green} >Condtion with Style </h1>        
-      <h2 style={{backgroundColor:color=='red'?'red':'green'}} >Heading 2</h2>         
+      <h2 style={{backgroundColor:color=='red'?'red':'green'}} >Heading 2</h2>           
       <h3 id={style.orange} >Heading 3</h3>  
   
       <h4  className={red}>Dummy text 1</h4>
