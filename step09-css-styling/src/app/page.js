6283b4fef3a9f9ad3365508e2c56cf07200ea5ed
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-12-10 00:31:43 +0500

    Update page.js

--- step09-css-styling/src/app/page.js
+++ step09-css-styling/src/app/page.js
@@ -7,7 +7,7 @@ export default function Home()
   const [color,setColor]=useState("red");  
   const {red} = style;          
       
-  return (  
+  return (    
     <main>    
       <h1 className={color=='red'?style.red:style.green} >Condtion with Style </h1>        
       <h2 style={{backgroundColor:color=='red'?'red':'green'}} >Heading 2</h2>         
