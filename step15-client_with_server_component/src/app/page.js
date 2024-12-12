4091926579faf530498ea5c16d1eaafca0d3174d
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-12-13 00:31:54 +0500

    Update page.js

--- step15-client_with_server_component/src/app/page.js
+++ step15-client_with_server_component/src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (  
     <>
-      <h1>Client Component with Server Component</h1>
+      <h1>Client Component with Server Component</h1>  
     </>  
   );
 }
