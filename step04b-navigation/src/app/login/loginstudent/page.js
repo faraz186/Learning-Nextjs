57bbe0916c2a68d737c74dec337b0f5021f55541
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-12-24 03:10:23 +0500

    Update page.js

--- step04b-navigation/src/app/login/loginstudent/page.js
+++ step04b-navigation/src/app/login/loginstudent/page.js
@@ -1,5 +1,5 @@
 "use client";        
-import { useRouter } from "next/navigation";     
+import { useRouter } from "next/navigation";        
   
 export default function LoginStudent() {  
   const router = useRouter();     
