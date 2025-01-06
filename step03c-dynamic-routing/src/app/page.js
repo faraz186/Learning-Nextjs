35b4f0d0c13690dafc00ff71591c223c2c4a3dd8
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-01-06 14:18:10 +0500

    dynamic routing b/w multiple routes

--- step03c-dynamic-routing/src/app/page.js
+++ step03c-dynamic-routing/src/app/page.js
@@ -2,7 +2,7 @@
 import { useRouter } from "next/navigation";          
   
 export default function Home() {
-  const router = useRouter();    
+  const router = useRouter();      
   
   return (              
     <div>     
