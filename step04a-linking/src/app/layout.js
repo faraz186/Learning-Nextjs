8653bc28e9b2baf0607366c57159fb15d43dfbce
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-01-13 08:24:52 +0500

    Update layout.js

--- step04a-linking/src/app/layout.js
+++ step04a-linking/src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";    
   
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });  
     
 export const metadata = {
   title: "step04a-Linking",    
