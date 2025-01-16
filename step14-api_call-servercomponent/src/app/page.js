1dcb568dc6bc012e8ed5ae0241786f6fa87f5331
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-01-16 19:59:52 +0500

    Update page.js

--- step14-api_call-servercomponent/src/app/page.js
+++ step14-api_call-servercomponent/src/app/page.js
@@ -5,7 +5,7 @@ export default function Home()
   return (        
     
     <div style={{marginLeft:"32%"}}>              
-    
+      
       <h1>API Calling with Server Component</h1>               
 
       <Link style={{marginLeft:"15%"}} href="/productlist">Go to Product List</Link>      
