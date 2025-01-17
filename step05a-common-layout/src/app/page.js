1480991dd05badadc8c5a1d4db460e5ba354c861
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-01-17 11:13:33 +0500

    Update page.js

--- step05a-common-layout/src/app/page.js
+++ step05a-common-layout/src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   const router = useRouter();
   const navigate = (name) => {  
     router.push(name);  
-  };
+  };   
   return (
     <main>     
       <h1>Common Layout Page</h1>  
