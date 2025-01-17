a9bc8dc9b0ae7baf39ad67549c221f12f751a778
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-01-17 11:13:41 +0500

    Update page.js

--- step05a-common-layout/src/app/page.js
+++ step05a-common-layout/src/app/page.js
@@ -1,6 +1,6 @@
 "use client";     
        
-import Link from "next/link";    
+import Link from "next/link";     
 import styles from "./page.module.css";  
 import { useRouter } from "next/navigation";     
   
