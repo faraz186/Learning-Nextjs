056adbd783e76ded6a0fc24542488b559319d3f7
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-01-17 11:14:57 +0500

    Update page.js

--- step05a-common-layout/src/app/page.js
+++ step05a-common-layout/src/app/page.js
@@ -5,7 +5,7 @@ import styles from "./page.module.css";
 import { useRouter } from "next/navigation";     
     
 export default function Home() {     
-  const router = useRouter();  
+  const router = useRouter();   
   const navigate = (name) => {     
     router.push(name);  
   };     
