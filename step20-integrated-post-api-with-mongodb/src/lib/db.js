39295b2f35b1cc6fe2bfa228adda045c09ad80e3
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-02-23 04:34:51 +0500

    Update db.js

--- step20-integrated-post-api-with-mongodb/src/lib/db.js
+++ step20-integrated-post-api-with-mongodb/src/lib/db.js
@@ -1,3 +1,3 @@
-const {username,password} = process.env;   
+const {username,password} = process.env;    
 
 export const connectionSrt = "mongodb+srv://"+username+":"+password+"@cluster0.mff4b5e.mongodb.net/?retryWrites=true&w=majority"
