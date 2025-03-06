9d819a0c23bce7bf8c62683517b00ced4002f63f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-03-06 12:45:25 +0500

    Update page.js

--- step04b-navigation/src/app/about/page.js
+++ step04b-navigation/src/app/about/page.js
@@ -1,5 +1,5 @@
 "use client";  
-import { useRouter } from "next/navigation";              
+import { useRouter } from "next/navigation";               
   
 export default function About() {  
   const router = useRouter();
