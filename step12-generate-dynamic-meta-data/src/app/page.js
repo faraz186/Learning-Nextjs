12c31f1e9f7fca38e8c0d3f84b1e2ebc4db4041d
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-04-14 20:02:52 +0500

    Update page.js

--- step12-generate-dynamic-meta-data/src/app/page.js
+++ step12-generate-dynamic-meta-data/src/app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {                              
   return (     
    <>
-      <h1 style={{textAlign:"center"}}>Main Home Page</h1>
+      <h1 style={{textAlign:"center"}}>Main Home Page</h1> s
+  
    </>
   )   
 }
